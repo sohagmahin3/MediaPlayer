34f84de0e328b3a58459627cb83b367cfd25f9eb
sohagmahin3 <sohagmahin3@gmail.com>
2019-02-23 16:36:45 +0600

    Modified Main UI added Move and Forward seekBar Set Toast when Music playing is finished.

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.mediaplayer;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener , MediaPlayer.OnCompletionListener {
+
+    //UI components
     private VideoView videoDisplayView;
     private Button btnVideoView;
-    private MediaController mediaController;
-    private MediaPlayer mediaPlayer;
-
     private Button btnPlayMusic, btnPauseMusic;
+    private SeekBar seekBarVolume;
+    private SeekBar seekBarmoveForward;
 
+    private MediaPlayer mediaPlayer;
+    private MediaController mediaController;
+    private AudioManager audioManager;
 
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +39,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         videoDisplayView = findViewById(R.id.videoView);
         btnVideoView = findViewById(R.id.btnVideo);
-        btnVideoView.setOnClickListener(this);
-
         btnPlayMusic = findViewById(R.id.btnPlayMusic);
         btnPauseMusic = findViewById(R.id.btnPauseMusic);
+        seekBarVolume = findViewById(R.id.seekBarVolume);
+        seekBarmoveForward = findViewById(R.id.mfSeekBar);
+
 
         mediaController = new MediaController(MainActivity.this);
+        audioManager =(AudioManager) getSystemService(AUDIO_SERVICE);
 
+        btnVideoView.setOnClickListener(this);
         btnPlayMusic.setOnClickListener(this);
         btnPauseMusic.setOnClickListener(this);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.freemusic);
 
+        int maxVolumeOfUserService = audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC);
+        int currentVolumeOfUserDevice = audioManager.getStreamVolume(audioManager.STREAM_MUSIC);
+
+        seekBarVolume.setMax(maxVolumeOfUserService);
+        seekBarVolume.setProgress(currentVolumeOfUserDevice);
+
+        seekBarmoveForward.setOnSeekBarChangeListener(this);
+        seekBarmoveForward.setMax(mediaPlayer.getDuration());
+        mediaPlayer.setOnCompletionListener(this);
+
+
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    Toast.makeText(MainActivity.this,Integer.toString(progress),Toast.LENGTH_SHORT).show();
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
 
@@ -56,12 +101,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.btnPlayMusic:
                 mediaPlayer.start();
+                timer = new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        seekBarmoveForward.setProgress(mediaPlayer.getCurrentPosition());
+                    }
+                },0,1000);
                 break;
 
             case R.id.btnPauseMusic:
                 mediaPlayer.pause();
                 break;
         }
+
     }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(fromUser) {
+            //Toast.makeText(this, Integer.toString(progress), Toast.LENGTH_SHORT).show();
+            mediaPlayer.seekTo(progress);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        mediaPlayer.pause();
+
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        timer.cancel();
+        Toast.makeText(this,"Music is finished !",Toast.LENGTH_SHORT).show();
+    }
 }
