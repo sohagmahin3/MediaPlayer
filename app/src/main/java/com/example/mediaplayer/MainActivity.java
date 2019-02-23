6003901538c8c5dce6ba1e7b8a99d8a88ea6a46f
sohagmahin3 <sohagmahin3@gmail.com>
2019-02-23 12:50:13 +0600

    Modify Layout Added play Music And Pause Music function

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mediaplayer;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +12,13 @@ import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private VideoView videoDisplayView;
-    private Button buttonView;
+    private Button btnVideoView;
     private MediaController mediaController;
+    private MediaPlayer mediaPlayer;
+
+    private Button btnPlayMusic, btnPauseMusic;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +26,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         videoDisplayView = findViewById(R.id.videoView);
-        buttonView = findViewById(R.id.button);
-        buttonView.setOnClickListener(this);
+        btnVideoView = findViewById(R.id.btnVideo);
+        btnVideoView.setOnClickListener(this);
+
+        btnPlayMusic = findViewById(R.id.btnPlayMusic);
+        btnPauseMusic = findViewById(R.id.btnPauseMusic);
 
         mediaController = new MediaController(MainActivity.this);
+
+        btnPlayMusic.setOnClickListener(this);
+        btnPauseMusic.setOnClickListener(this);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.freemusic);
+
     }
 
-    @Override
-    public void onClick(View v) {
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
-        videoDisplayView.setVideoURI(uri);
-        videoDisplayView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoDisplayView);
-        videoDisplayView.start();
 
+    @Override
+    public void onClick(View ButtonView) {
+
+        switch(ButtonView.getId()){
+            case R.id.btnVideo:
+                Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
+                videoDisplayView.setVideoURI(uri);
+                videoDisplayView.setMediaController(mediaController);
+                mediaController.setAnchorView(videoDisplayView);
+                videoDisplayView.start();
+                break;
+
+            case R.id.btnPlayMusic:
+                mediaPlayer.start();
+                break;
+
+            case R.id.btnPauseMusic:
+                mediaPlayer.pause();
+                break;
+        }
     }
 
 }
